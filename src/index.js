608cab6491839ff81ba81c1c3004229ea913fb1a
Eber Cintra <organico_51@hotmail.com>
2025-10-17 00:25:55 -0300

    criando estrutura do projeto

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => ({ message: "backend E-commerce rodando!" }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
